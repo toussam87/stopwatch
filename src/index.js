8d79979534f51184039fa69a76c3a9384d37b55c
Annie <toussaint.am@gmail.com>
2019-09-28 17:47:14 -0400

    initial amount of work complete. Working on adjusting seconds so the number resets at 60 seconds

--- src/index.js
+++ src/index.js
@@ -1,8 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Index = () => {
-  return <div>Hello React!</div>;
+class App extends React.Component  {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      seconds: 0,
+      minutes: 0
+    }
+
+    this.elpasedTime
+  } 
+
+  getTime(){
+    let startTime = Date.now();
+    this.elpasedTime = setInterval(() => {
+      let setSeconds = Math.round((Date.now() - startTime)/1000);
+      this.setState({seconds: setSeconds});
+      
+      this.state.seconds % 60 === 0 ? this.setState(prevState => { return {minutes: prevState.minutes + 1 }}) : ''
+    }, 1000)
+  }
+  
+  stopTime(){
+    clearInterval(this.elpasedTime)
+    this.setState({seconds: 0, minutes: 0})
+  }
+
+  render() {
+    return (
+    <div>
+      <h1>Hello</h1>
+      <p>Press the button and watch the seconds, and then minutes, go by</p>
+      <span>Minutes: {this.state.minutes} </span> 
+      <span>Seconds: {this.state.seconds} </span>
+      <button onClick={e => this.getTime()}>Start</button>
+      <button onClick={e => this.stopTime()}>Stop</button>
+    </div>
+      );
+  }
 };
 
-ReactDOM.render(<Index />, document.getElementById("index"));
+ReactDOM.render(<App />, document.getElementById("index"));
